53309d55c40ecf82fac8806780d2c30ad4106757
Soar <85624529@qq.com>
2016-06-20 21:40:42 +0800

    [optimize] change the config

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Open
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+      config.action_controller.asset_host = Settings.asset_host 
       config.i18n.default_locale = :"zh-CN"
   end
 end
